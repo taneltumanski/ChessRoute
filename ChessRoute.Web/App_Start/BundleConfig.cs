b7af17f3e8e49224d56dd33a03c7333053f4693e
Tanel <tanel.tumanski@gmail.com>
2015-06-28 21:12:09 +0300

    Updated to VS 2015 and updated the nuget packages. Also added a JS chess library

--- ChessRoute.Web/App_Start/BundleConfig.cs
+++ ChessRoute.Web/App_Start/BundleConfig.cs
@@ -18,26 +18,18 @@ namespace ChessRoute.Web
 						"~/Scripts/jquery.unobtrusive*",
 						"~/Scripts/jquery.validate*"));
 
+			bundles.Add(new ScriptBundle("~/bundles/chess").Include(
+						"~/Scripts/ChessBoard/chessboard-{version}.js"));
+
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Content/chess").Include("~/Content/chessboard-{version}.css"));
 
-			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-						"~/Content/themes/base/jquery.ui.core.css",
-						"~/Content/themes/base/jquery.ui.resizable.css",
-						"~/Content/themes/base/jquery.ui.selectable.css",
-						"~/Content/themes/base/jquery.ui.accordion.css",
-						"~/Content/themes/base/jquery.ui.autocomplete.css",
-						"~/Content/themes/base/jquery.ui.button.css",
-						"~/Content/themes/base/jquery.ui.dialog.css",
-						"~/Content/themes/base/jquery.ui.slider.css",
-						"~/Content/themes/base/jquery.ui.tabs.css",
-						"~/Content/themes/base/jquery.ui.datepicker.css",
-						"~/Content/themes/base/jquery.ui.progressbar.css",
-						"~/Content/themes/base/jquery.ui.theme.css"));
+			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include("~/Content/themes/base/*.css"));
 		}
 	}
 }
---
+++ ChessRoute.Web/Models/ChessParameterModel.cs
@@ -0,0 +1,35 @@
+﻿using ChessRoute.Solver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChessRoute.Web.Models
+{
+	public class ChessParameterModel
+	{
+		[Required]
+		[Display(Name = "Start position")]
+		public string StartPosition { get; set; }
+
+		[Required]
+		[Display(Name = "End position")]
+		public string EndPosition { get; set; }
+
+		[Required]
+		[Display(Name = "Chess piece")]
+		public ChessPieceOption ChessPiece { get; set; }
+
+		[Required]
+		public IEnumerable<string> TakenPositions { get; set; }
+
+		[Required]
+		[Display(Name = "Board width")]
+		public int BoardWidth { get; set; }
+
+		[Required]
+		[Display(Name = "Board height")]
+		public int BoardHeight { get; set; }
+	}
+}
