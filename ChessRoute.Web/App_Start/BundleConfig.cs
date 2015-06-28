cee8be17b7bec6342faabba817b298f67ff31e65
Tanel <tanel.tumanski@gmail.com>
2015-06-28 22:10:09 +0300

    Removed the chess library

--- ChessRoute.Web/App_Start/BundleConfig.cs
+++ ChessRoute.Web/App_Start/BundleConfig.cs
@@ -18,16 +18,12 @@ namespace ChessRoute.Web
 						"~/Scripts/jquery.unobtrusive*",
 						"~/Scripts/jquery.validate*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/chess").Include(
-						"~/Scripts/ChessBoard/chessboard-{version}.js"));
-
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-			bundles.Add(new StyleBundle("~/Content/chess").Include("~/Content/chessboard-{version}.css"));
 
 			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include("~/Content/themes/base/*.css"));
 		}
